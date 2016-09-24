a1d87169998e7c81123b3fc8597a2007b34881c1
Marlon <Marlon@DESKTOP-ED65MII>
2016-09-24 11:10:34 +1000

    pre-adding job dropdown

--- Careermatcher/Controllers/JobController.cs
+++ Careermatcher/Controllers/JobController.cs
@@ -13,7 +13,13 @@ namespace Careermatcher.Controllers
     public class JobController : Controller
     {
         private JobDBContext db = new JobDBContext();
-        public enum items { Action=0, Drama, Comedy, Science_Fiction };
+        public enum items {
+            Primary_School_Completion=1, Yr_10_Equivalent_Completion, Yr_12_Equivalent_Completion,
+            VET_Certificate_I_to_IV, VET_Diploma, VET_Advanced_diploma, VET_Vocational_graduate_certificate, TAFE_Diploma,
+            TAFE_Advanced_diploma, TAFE_Vocational_graduate_certificate, Bachelor_degree, Bachelor_degree_honours_, Masters_degree, Doctoral_degree, Accounting, Business_policy_and_strategy, Data_Analysis_And_Statistics, Economics, Finance,
+            Global_Business, Human_Resources_Management, Leadership, Marketing, Operations, Organisational_Behaviour
+        };
+
         // GET: Job
         public ActionResult Index()
         {
@@ -32,7 +38,40 @@ namespace Careermatcher.Controllers
         [HttpPost]
         public String Index(items[] groupselect)
         {
-            return "yay!!";
+            String text = "";
+            for(int i=0;i<groupselect.Length;i++)
+            {
+                text+=groupselect[i].ToString()+",";
+            }
+            return text;
+        }
+        public ActionResult Index2()
+        {
+            DropDown objData = new DropDown();
+            //create object data for DropdownModel
+            objData.ParentDataModel = new List<Parent>();
+            objData.ClildDataModel = new List<Child>();
+
+            objData.ParentDataModel = GetParentData();
+            objData.ClildDataModel = GetChildData();
+
+
+
+            return View(objData);
+        }
+
+
+        public ActionResult getJobtags()
+        {
+            DropDown objData = new DropDown();
+            //create object data for DropdownModel
+            objData.ParentDataModel = new List<Parent>();
+            objData.ClildDataModel = new List<Child>();
+
+            objData.ParentDataModel = GetParentData_Jobs();
+            objData.ClildDataModel = GetChildData_Jobs();
+
+            return View(objData);
         }
 
         // GET: Job/Details/5
@@ -179,6 +218,60 @@ namespace Careermatcher.Controllers
             lstChild.Add(new Child { ParentId = 5, ChildId = 23, ChildName = "Marketing" });
             lstChild.Add(new Child { ParentId = 5, ChildId = 24, ChildName = "Operations" });
             lstChild.Add(new Child { ParentId = 5, ChildId = 25, ChildName = "Organisational Behaviour" });
+
+            return lstChild;
+        }
+        private List<Parent> GetParentData_Jobs()
+        {
+            List<Parent> lstParent = new List<Parent>();
+            lstParent.Add(new Parent { ParentId = 1, ParentName = "Agriculture, Food and Natural Resources" });
+            lstParent.Add(new Parent { ParentId = 2, ParentName = "Architecture and Contruction" });
+            lstParent.Add(new Parent { ParentId = 3, ParentName = "Arts, Audio, Video Technology Communications:" });
+            lstParent.Add(new Parent { ParentId = 4, ParentName = "Business Management and Administration" });
+            lstParent.Add(new Parent { ParentId = 5, ParentName = "Finance" });
+            lstParent.Add(new Parent { ParentId = 5, ParentName = "Government and Public Administration" });
+            lstParent.Add(new Parent { ParentId = 5, ParentName = "Health Science" });
+            lstParent.Add(new Parent { ParentId = 5, ParentName = "Information Technology:" });
+
+
+            return lstParent;
+        }
+        private List<Child> GetChildData_Jobs()
+        {
+            
+            List<Child> lstChild = new List<Child>();
+            lstChild.Add(new Child { ParentId = 1, ChildId = 1, ChildName = "Agricultural Equipment Operator" });
+            lstChild.Add(new Child { ParentId = 1, ChildId = 2, ChildName = "Agricultural Inspector" });
+            lstChild.Add(new Child { ParentId = 1, ChildId = 3, ChildName = "Butcher" });
+            lstChild.Add(new Child { ParentId = 2, ChildId = 4, ChildName = "Architect" });
+            lstChild.Add(new Child { ParentId = 2, ChildId = 5, ChildName = "Carpenter" });
+            lstChild.Add(new Child { ParentId = 2, ChildId = 6, ChildName = "Concrete Finisher" });
+            lstChild.Add(new Child { ParentId = 2, ChildId = 7, ChildName = "Civil Engineer" });
+            lstChild.Add(new Child { ParentId = 3, ChildId = 8, ChildName = "Actor" });
+            lstChild.Add(new Child { ParentId = 3, ChildId = 9, ChildName = "Audio and Video Equipment Technician" });
+            lstChild.Add(new Child { ParentId = 3, ChildId = 10, ChildName = "Broadcast Technician" });
+            lstChild.Add(new Child { ParentId = 4, ChildId = 11, ChildName = "Accountant" });
+            lstChild.Add(new Child { ParentId = 4, ChildId = 12, ChildName = "Advertising Sales Agent" });
+            lstChild.Add(new Child { ParentId = 4, ChildId = 13, ChildName = "Auditor" });
+            lstChild.Add(new Child { ParentId = 4, ChildId = 14, ChildName = "Budget Analyst" });
+            lstChild.Add(new Child { ParentId = 5, ChildId = 15, ChildName = "Accounting" });
+            lstChild.Add(new Child { ParentId = 5, ChildId = 16, ChildName = "Bill and Account Collector" });
+            lstChild.Add(new Child { ParentId = 5, ChildId = 17, ChildName = "Credit Analyst" });
+            lstChild.Add(new Child { ParentId = 5, ChildId = 18, ChildName = "Financial Analyst" });
+            lstChild.Add(new Child { ParentId = 5, ChildId = 19, ChildName = "Investment Fund Manager" });
+            lstChild.Add(new Child { ParentId = 6, ChildId = 20, ChildName = "Air Crew Member" });
+            lstChild.Add(new Child { ParentId = 6, ChildId = 21, ChildName = "Economist" });
+            lstChild.Add(new Child { ParentId = 6, ChildId = 22, ChildName = "Financial Examiner" });
+            lstChild.Add(new Child { ParentId = 6, ChildId = 23, ChildName = "Meter Reader" });
+            lstChild.Add(new Child { ParentId = 6, ChildId = 24, ChildName = "Postal Service Clerk" });
+            lstChild.Add(new Child { ParentId = 6, ChildId = 25, ChildName = "Postal Service Mail Carrier" });
+            lstChild.Add(new Child { ParentId = 7, ChildId = 26, ChildName = "Acupuncturist" });
+            lstChild.Add(new Child { ParentId = 7, ChildId = 27, ChildName = "Anesthesiologist" });
+            lstChild.Add(new Child { ParentId = 7, ChildId = 28, ChildName = "Biologist" });
+            lstChild.Add(new Child { ParentId = 7, ChildId = 29, ChildName = "Chiropractor" });
+            lstChild.Add(new Child { ParentId = 7, ChildId = 30, ChildName = "Dental Assisstant" });
+            lstChild.Add(new Child { ParentId = 7, ChildId = 31, ChildName = "Dentist" });
+            lstChild.Add(new Child { ParentId = 8, ChildId = 32, ChildName = "Business Intelligence Analyst" });
 
             return lstChild;
         }
